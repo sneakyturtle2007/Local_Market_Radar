58695ae02c94ab1c5348c1e69e6e1e9754a86d3d
johnAppDev <99500965+johnAppDev@users.noreply.github.com>
2023-11-26 18:59:20 -0600

    Added first iteration http server files

---
+++ Handler.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.io.OutputStream;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+public class Handler implements HttpHandler{
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = "This is the response";
+            t.sendResponseHeaders(200, response.getBytes().length);
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+            System.out.print("Connection");
+    }
+}
---
+++ ServerMain.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+public class ServerMain {
+    public static void main(String[] args) throws IOException {
+      HttpServer httpServer = HttpServer.create(new InetSocketAddress("localhost", 8000), 0);
+      httpServer.createContext("/test", new Handler());
+      httpServer.start();
+      
+    }
+    
+}
+
