d077bc91d778391b6933cc0ff83294d9318ec08d
Luis Blake <103067582+sneakyturtle2007@users.noreply.github.com>
2023-11-27 19:59:23 -0600

    adding the server

--- Handler.java
+++ Handler.java
@@ -1,17 +1,25 @@
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
+public class Handler implements HttpHandler {
 
-public class Handler implements HttpHandler{
-        @Override
-        public void handle(HttpExchange t) throws IOException {
-            String response = "This is the response";
-            t.sendResponseHeaders(200, response.getBytes().length);
-            OutputStream os = t.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
-            System.out.print("Connection");
+  @Override
+  public void handle(HttpExchange t) throws IOException {
+    if (t.getRequestURI().toString().equals("/test")) {
+      File main_html = new File("index.html");
+      t.sendResponseHeaders(200, main_html.length());
+      OutputStream os = t.getResponseBody();
+      os.write(main_html.toString().getBytes());
+      os.close();
+    } else if (t.getRequestURI().toString().equals("/test2")) {
+      File main_html = new File("index2.html");
+      t.sendResponseHeaders(200, main_html.length());
+      OutputStream os = t.getResponseBody();
+      os.write(main_html.toString().getBytes());
+      os.close();
     }
+  }
 }
--- ServerMain.java
+++ ServerMain.java
@@ -1,18 +1,19 @@
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
-
 public class ServerMain {
-    public static void main(String[] args) throws IOException {
-      HttpServer httpServer = HttpServer.create(new InetSocketAddress("localhost", 8000), 0);
-      httpServer.createContext("/test", new Handler());
-      httpServer.start();
-      
-    }
-    
-}
 
+  public static void main(String[] args) throws IOException {
+    HttpServer httpServer = HttpServer.create(
+      new InetSocketAddress("localhost", 8000),
+      0
+    );
+    httpServer.createContext("/test", new Handler());
+    httpServer.createContext("/test2", new Handler());
+    httpServer.start();
+  }
+}
