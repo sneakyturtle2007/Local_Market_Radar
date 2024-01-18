98e493b77a58141820159cce502f002609b6a16e
Luis Blake <103067582+sneakyturtle2007@users.noreply.github.com>
2024-01-18 12:32:48 -0600

    trying to add html

--- Handler.js
+++ Handler.js
@@ -1,19 +1,11 @@
-var dt = require('./Handler');
+
 var url = require('url');
 var filesystem = require('fs');
 
 exports.importHtmlFiles = function (res) {
     filesystem.readFile('FrontEnd/index.html', function(err, data) {
-        res.write(data);
-    });
-    filesystem.readFile('FrontEnd/BetterFrontPage.html', function(err, data) {
-        res.write(data);
-    });
-    filesystem.readFile('FrontEnd/Business_Page.html', function(err, data) {
-        res.write(data);
-    });
-    filesystem.readFile('FrontEnd/Account.html', function(err, data) {
-        res.write(data);
-    });
-    return res.end();
+        if(err) throw err;
+        
+        return data;
+    });  
 };
--- ServerMain.js
+++ ServerMain.js
@@ -1,10 +1,33 @@
 var http = require('http');
-var dt = require('./Handler');
+var path = require('path');
 var url = require('url');
 var filesystem = require('fs');
+var handler = require('./Handler.js');
+server = http.createServer(function (req, res) {
+  
+  var urlPath = url.parse(req.url).pathname;
+  var htmlFiles = ['Account.html', 'index.html', 'BetterFrontPage.html', 'Business_Page.html']
+  if(urlPath == '/') {
+    urlPath = path.join(urlPath, 'index.html');
+  }
+  if (urlPath in htmlFiles){
+    var htmlpath = path.join(__dirname, 'FrontEnd', urlPath);
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
+    console.log(htmlpath);
+
+    filesystem.readFile(htmlpath, function(err, data) {
+
+      if(err) throw err;
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
+  }
   
   
+  //res.writeHead(200, {'Content-Type': 'text/html'});
+  //res.write();
+  //res.end(handler.importHtmlFiles(res));
+  //var urlPath = url.parse(req.url).pathname;
+  //var htmlPath = path.join( 'FrontEnd/', urlPath, '.html');
 }).listen(8000);
