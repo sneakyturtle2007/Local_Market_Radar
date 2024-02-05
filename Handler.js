fb7ff2af8a666082f1060d0c69ef033ed93e5c24
Luis Blake <103067582+sneakyturtle2007@users.noreply.github.com>
2024-02-04 22:03:09 -0600

    structure changes to code to try and get sql query to work

--- Handler.js
+++ Handler.js
@@ -1,13 +1,19 @@
+const database = require('./Handler.js');
+
 function addItem() {
-    console.log("Handler.js running")
+    
+    let accounst = database.getaccounts();
     let price_and_stock = document.getElementById("itemTemplate");
     let footer_template = document.getElementById("footerTemplate");
+
     let footer = document.getElementById("footer");
     let main = document.getElementById("main");
+
     for (let i = 0; i < 100; i++) {
         let clon = price_and_stock.content.cloneNode(true);
         let itemprice_itemstock = clon.querySelector("p");
-        itemprice_itemstock.textContent = "Price: " + i + " | Stock: " + i;
+        let textcontent1 = "Price: " + i + " | Stock: " + i + accounst ;
+        itemprice_itemstock.textContent = textcontent1;
 
         main.appendChild(clon);
         
--- ServerMain.js
+++ ServerMain.js
@@ -1,8 +1,48 @@
-var http = require('http');
+var express = require('express');
 var path = require('path');
-var url = require('url');
 var filesystem = require('fs');
+const mysql = require('mysql2');
+var app = express();
 
+
+app.use(express.static(path.join(__dirname, 'FrontEnd')));
+
+/*app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'FrontEnd', 'BetterFrontPage.html'));
+});*/
+
+app.get('/Handler.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'Handler.js'));
+});
+
+app.listen(8000, function () {
+  console.log('App listening on port 8000!');
+});
+
+
+
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "Main"
+});
+var accresult;
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+ 
+}); 
+con.query("SELECT * FROM accounts", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+    accresult = result;
+    
+});
+
+
+/*
 server = http.createServer(function (req, res) {
   
   var urlPath = url.parse(req.url).pathname;
@@ -21,7 +61,7 @@ server = http.createServer(function (req, res) {
     console.log(htmlpath);
 
     filesystem.readFile(htmlpath, function(err, data) {
-      if(err) throw err;
+      if(err) console.log(err);
       res.writeHead(200, {'Content-Type': 'text/html'});
       res.write(data);
       res.end();
@@ -29,7 +69,7 @@ server = http.createServer(function (req, res) {
     
   }else if (urlPath === '/Handler.js') {
     filesystem.readFile('Handler.js', 'utf8', (err, data) => {
-      if(err) throw err;
+      if(err) console.log(err);
       res.writeHead(200, { 'Content-Type': 'application/javascript' });
       res.write(data);
       res.end();
@@ -37,3 +77,4 @@ server = http.createServer(function (req, res) {
   }
 
 }).listen(8000);
+*/
