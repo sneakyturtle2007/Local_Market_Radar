8b08c7d45a26bc71c80756301431615e919dcd35
Luis Blake <103067582+sneakyturtle2007@users.noreply.github.com>
2024-02-25 10:27:01 -0600

    Fixed item image displaying
    Fixed the bug in which the image wouldn't show it was caused by a syntax error and mistype of the constructor of the item class.

--- ServerMain.js
+++ ServerMain.js
@@ -12,8 +12,8 @@ class item{
     this.image = image;
   }
 }
-function createItem(name, price, description, image){
-  return new item(name, price, description, image);
+function createItem(name, price, stock, description, image){
+  return new item(name, price,stock, description, image);
 }
 
 app.set('view engine', 'ejs');
@@ -29,7 +29,7 @@ app.get('/', function (req, res) {
 app.get('/itempage', function (req, res) {
   var items = [];
   for(var i = 0; i < 10; i++){
-    items.push(createItem('Item' + i, 100 + i, 0,  'This is item ' + i, "https://i.ytimg.com/vi/_3OUQTruQRE/maxresdefault.jpg" ));
+    items.push(createItem('Item' + i, 100 + i, 0 ,  'This is item ' + i, 'https://i.ytimg.com/vi/_3OUQTruQRE/maxresdefault.jpg' ));
   }
 
 
