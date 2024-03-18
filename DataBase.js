50e0f735f6cf8f132c5f0b300611f52eb09a24fd
Luis Blake <103067582+sneakyturtle2007@users.noreply.github.com>
2024-03-18 03:28:12 -0500

    Added deleteProduct for deleting products from the database.

--- DataBase.js
+++ DataBase.js
@@ -30,12 +30,23 @@
     }
 // FUNCTION: addProduct
     async function addProduct( ProductName, ProductPrice, ProductStock, ProductDescription) {
-        let sql = "INSERT INTO products ( ProductName, ProductPrice, ProductStock, ProductDescription) VALUES (" + BusinessID + "', '" + ProductName + "', '" + ProductPrice + "', '" + ProductStock + "', '" + ProductDescription + "')";
+        let sql = "INSERT INTO products ( ProductName, ProductPrice, ProductStock, ProductDescription) VALUES ('" + ProductName + "', '" + ProductPrice + "', '" + ProductStock + "', '" + ProductDescription + "')";
         let [results, fields] = await con.promise().query(sql).catch((err) => { console.log(err); });
-        if(results.affectedRows > 0){
+        if(await results.affectedRows > 0){
             return true;
         }
         return false;
     }
+// FUNCTION: deleteProduct
+    async function deleteProduct(ProductName) {
+        let sql = "DELETE FROM products WHERE ProductName = " + "'" + ProductName + "'";
+        let [results, fields] = await con.promise().query(sql).catch((err) => { console.log(err); });
+        if(await results.affectedRows > 0){
+            return true;
+        }
+        return false;
+    }
+//console.log(addProduct('test', 1.99, 100, 'test'));
+
 // EXPORTS
     module.exports = {getProducts};
