aaac45ad09fe94ff673a6b0514c39f901e0ff24c
Luis Blake <103067582+sneakyturtle2007@users.noreply.github.com>
2024-04-17 18:20:27 -0500

    Back button on profile and profile info.

--- Profile/Profile.js
+++ Profile/Profile.js
@@ -3,15 +3,54 @@ async function getProfile(){
     var username;
     var cookies = document.cookie.split(";");
     var username = cookies[1].split("=")[1];
+
+    var backButton = document.getElementById("backButton");
+    backButton.onclick = function(){
+        document.location.href = '/Search';
+    }
+
     var profile = await fetch("/api/profile?username=" + username).then(res => res.json()).catch(err => console.log(err) );
-    document.getElementsByClassName("profilePicture").src = profile.AccountProfilePicture;
-    document.getElementById("Username").textContent = username;
-    document.getElementById("Email").textContent = profile.AccountEmail;
+    
+    var profileContainer = document.getElementById("profile");
+    profileContainer.innerHTML = "";
+    
+    var profilePicture_and_username = document.createElement("div");
+    profilePicture_and_username.id = "profilePicture_and_username";
+
+    var profilePicture = document.createElement("img");
+    profilePicture.className = "profilePicture";
+    profilePicture.src = profile.AccountProfilePicture;
+
+    var username_and_Email = document.createElement("div");
+    username_and_Email.id = "Username_and_Email";
+
+    var Username = document.createElement("h3");
+    Username.id = "Username";
+    Username.textContent = username;
+
+    var Email = document.createElement("p");
+    Email.id = "Email";
+    Email.textContent = profile.AccountEmail;
+
+    var editProfileLink = document.createElement("a");
+    editProfileLink.id = "EditProfile";
+    editProfileLink.href = "/EditProfile";
+
+    username_and_Email.appendChild(Username);
+    username_and_Email.appendChild(Email);
+    username_and_Email.appendChild(editProfileLink);
+    profilePicture_and_username.appendChild(profilePicture);
+    profilePicture_and_username.appendChild(username_and_Email);
+    profileContainer.appendChild(profilePicture_and_username);
 } 
 async function openProfileInfo(){
     var cookies = document.cookie.split(";");
     var username = cookies[1].split("=")[1];
     
+    var backButton = document.getElementById("backButton");
+    backButton.onclick = function(){
+        getProfile();
+    }
     var profile = await fetch("/api/profile?username=" + username).then(res => res.json()).catch(err => console.log(err) );
     
     var profileContainer = document.getElementById("profile");
